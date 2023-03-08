0e13991000b73bac68a0379af3fe7e7b96990fae
Zain Khan <khan711.sheridancollege.ca>
2023-03-08 15:40:22 -0500

    I have wrote the comments to see if this works for the midterm.

---
+++ src/Zain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Zain
+{
+    public static void main(String[] args)
+    {
+        System.out.println("testing, testing, 1, 2, 3...");
+    }
+}
